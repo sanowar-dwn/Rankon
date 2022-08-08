9ee67c0347d580a2caa60b2cfaa83662a5fb1435
Sanowar Dewan <sanowardwnofficial@gmail.com>
2022-08-08 21:14:55 +0600

    all pages done

--- js/custom.js
+++ js/custom.js
@@ -13,16 +13,16 @@ $(function(){
           {
             breakpoint: 1024,
             settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
+              slidesToShow: 4,
+              slidesToScroll: 2,
               infinite: true,
-              dots: true
+              dots: false
             }
           },
           {
-            breakpoint: 600,
+            breakpoint: 769,
             settings: {
-              slidesToShow: 2,
+              slidesToShow: 3,
               slidesToScroll: 2
             }
           },
@@ -60,17 +60,16 @@ $(function(){
           {
             breakpoint: 1024,
             settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
+              slidesToShow: 1,
+              slidesToScroll: 1,
               infinite: true,
-              dots: true
             }
           },
           {
             breakpoint: 600,
             settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2
+              slidesToShow: 1,
+              slidesToScroll: 1
             }
           },
           {
@@ -124,4 +123,6 @@ $(function(){
           // instead of a settings object
         ]
       });
+      //VenoBox js Here
+      $('.venobox').venobox(); 
 })
